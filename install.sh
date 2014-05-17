629b67d6bfc80b6cc82b50e3ec5e0f0869766449
Amjith Ramanujam <amjith.r@gmail.com>
2014-05-17 17:59:24 +0200

    Update some git configs and add an install script.

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+ln -s $HOME/Dropbox/_dotties/zshrc $HOME/.zshrc
+ln -s $HOME/Dropbox/_dotties/zsh_functions $HOME/.zsh_functions
+ln -s $HOME/Dropbox/_dotties/zsh_aliases $HOME/.zsh_aliases
+ln -s $HOME/Dropbox/_dotties/zprofile $HOME/.zprofile
+
+ln -s $HOME/Dropbox/_dotties/gitignore $HOME/.gitignore
+ln -s $HOME/Dropbox/_dotties/gitconfig $HOME/.gitconfig
+ln -s $HOME/Dropbox/_dotties/tmux.conf $HOME/.tmux.conf
