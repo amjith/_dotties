728bee1951fb98cf2e3b66e4cfc679fcaabbcc16
Amjith Ramanujam <amjith.r@gmail.com>
2014-05-18 20:38:43 +0200

    Remove zprofile sym linking.

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 ln -s $HOME/Dropbox/_dotties/zshrc $HOME/.zshrc
 ln -s $HOME/Dropbox/_dotties/zsh_functions $HOME/.zsh_functions
 ln -s $HOME/Dropbox/_dotties/zsh_aliases $HOME/.zsh_aliases
-ln -s $HOME/Dropbox/_dotties/zprofile $HOME/.zprofile
 
 ln -s $HOME/Dropbox/_dotties/gitignore $HOME/.gitignore
 ln -s $HOME/Dropbox/_dotties/gitconfig $HOME/.gitconfig
